b4c6d145cc30f578188913cd008f1f44256dad6c
intiserahmed <ei.ahmed@live.iium.edu.my>
2023-03-19 13:30:53 +0800

    changing Hz sampling rate and storing reading associated with the current timestamp

--- spectreora-IoT/main.py
+++ spectreora-IoT/main.py
@@ -1,26 +1,136 @@
-import time
-from machine import ADC
+import utime
+from machine import Pin, ADC
+import network
+import ufirestore as firebase
+import config
+import ntptime
+
+
+# Set the NTP server address
+ntp_server = '0.pool.ntp.org'
 
 # Initialize the ADC on pin 26
 adc = ADC(26)
 
-# Initialize the counter to 0
+# Set up the Wi-Fi connection
+ssid = config.SSID
+password = config.PASSWORD
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(ssid, password)
+
+# Wait for connection or fail
+max_wait = 20
+while max_wait > 0:
+    if wlan.isconnected():
+        break
+    max_wait -= 1
+    print('Waiting for Wi-Fi connection...')
+    utime.sleep(1)
+
+if not wlan.isconnected():
+    raise RuntimeError('Wi-Fi connection failed')
+
+print('Wi-Fi connected')
+print('IP address:', wlan.ifconfig()[0])
+
+# Synchronize the time with an NTP server
+ntptime.host = ntp_server
+ntptime.settime()
+
+
+def get_current_timestamp():
+    # Get the current time tuple
+    current_time_tuple = utime.localtime()
+
+    # Convert the time tuple to a timestamp in seconds since the epoch
+    timestamp = utime.mktime(current_time_tuple)
+
+    # Add the timezone offset to the timestamp if necessary
+    timezone_offset = 8 * 3600  # 8 hours ahead of UTC
+    timestamp += timezone_offset
+
+    return timestamp
+
+
+# Initialize counter
 counter = 0
 
+
+# Firebase URl Setup
+firebase.setURL(config.FIREBASE_URL)
+
+
+# Initialize the ECG data dictionary
+ecg_data_array = []
+
+
+def format_timestamp(timestamp):
+
+    t = utime.localtime(get_current_timestamp())
+    formatted_time = "{:02}:{:02}:{:02} {:02}-{:02}-{:04}".format(
+        t[3], t[4], t[5], t[2], t[1], t[0])
+    return formatted_time
+
+
+# Format the timestamp
+session_time = "Session of " + format_timestamp(get_current_timestamp())
+
+try:
+    firebase.put(session_time, get_current_timestamp(), bg=0)
+except Exception as e:
+    print(e)
+
+led = machine.Pin("LED", machine.Pin.OUT)  # Set onboard LED pin
+
 while True:
+    if wlan.isconnected():
+        led.high()
+    else:
+        led.toggle()
+
     # Read the ECG data from the ADC
     ecg_data = adc.read_u16()
 
-    # Print the ECG data to the console
-    print(ecg_data)
+    # Corresponding to the sampling rate of 250 Hz
+    if counter < 250:
+        formatted_time = format_timestamp(get_current_timestamp())
 
-    # Increment the counter by 1
+        ecg_reading_json = {
+            # Each value will be stored and referenced as key
+            formatted_time: ecg_data,
+        }
+
+    ecg_data_array.append(ecg_reading_json)
+
+    # If 1 second have elapsed, store the ECG data to Firebase Realtime Database
+    if counter == 250:
+        # Get the current timestamp in seconds
+        formatted_time = format_timestamp(get_current_timestamp())
+
+        # Store the ECG data to Firebase Realtime Database with the timestamp
+        ecg_data_json = {
+            "timestamp": formatted_time,
+            'ecg_data': ecg_data_array}
+
+        try:
+            print("addTo")
+            firebase.addto(session_time,
+                           ecg_data_json, bg=0)
+
+        except Exception as e:
+            print(e)
+
+        # Clear the ECG data array
+        ecg_data_array.clear()
+
+    # Increment the counter
     counter += 1
 
-    # Check if 1 second has passed
-    if counter == 250:
-        print("1 second has passed")
+    # If the counter has reached 250, reset it to 0
+    if counter > 250:
         counter = 0
 
-    # Delay for 4 milliseconds (corresponding to the sampling rate of 250 Hz)
-    time.sleep(0.004)
+    # Delay for 4 milliseconds
+    utime.sleep(0.004)
---
+++ spectreora-IoT/ufirestore.py
@@ -0,0 +1,285 @@
+import ujson
+import usocket
+import ussl
+import _thread
+import time
+
+
+class FIREBASE_GLOBAL_VAR:
+    GLOBAL_URL = None
+    GLOBAL_URL_ADINFO = None
+    SLIST = {}
+
+
+class INTERNAL:
+    def connect(id):
+        LOCAL_ADINFO = usocket.getaddrinfo(
+            FIREBASE_GLOBAL_VAR.GLOBAL_URL_ADINFO["host"], FIREBASE_GLOBAL_VAR.GLOBAL_URL_ADINFO["port"], 0, usocket.SOCK_STREAM)[0]
+        FIREBASE_GLOBAL_VAR.SLIST["S"+id] = usocket.socket(
+            LOCAL_ADINFO[0], LOCAL_ADINFO[1], LOCAL_ADINFO[2])
+        FIREBASE_GLOBAL_VAR.SLIST["S"+id].connect(LOCAL_ADINFO[-1])
+        if FIREBASE_GLOBAL_VAR.GLOBAL_URL_ADINFO["proto"] == "https:":
+            try:
+                FIREBASE_GLOBAL_VAR.SLIST["SS"+id] = ussl.wrap_socket(
+                    FIREBASE_GLOBAL_VAR.SLIST["S"+id], server_hostname=FIREBASE_GLOBAL_VAR.GLOBAL_URL_ADINFO["host"])
+            except:
+                print("ENOMEM, try to restart. If you make to many id's (sokets) simultaneously (bg=1 and id=x), try to use less or use a board with more ram!\nSome emulation software limits the RAM.")
+                FIREBASE_GLOBAL_VAR.SLIST["S"+id].close()
+                FIREBASE_GLOBAL_VAR.SLIST["SS"+id] = None
+                FIREBASE_GLOBAL_VAR.SLIST["S"+id] = None
+                raise MemoryError
+
+        else:
+            FIREBASE_GLOBAL_VAR.SLIST["SS" +
+                                      id] = FIREBASE_GLOBAL_VAR.SLIST["S"+id]
+
+    def disconnect(id):
+        FIREBASE_GLOBAL_VAR.SLIST["SS"+id].close()
+        FIREBASE_GLOBAL_VAR.SLIST["SS"+id] = None
+        FIREBASE_GLOBAL_VAR.SLIST["S"+id] = None
+
+    def put(PATH, DATA, id, cb):
+        try:
+            while FIREBASE_GLOBAL_VAR.SLIST["SS"+id]:
+                time.sleep(2)
+            FIREBASE_GLOBAL_VAR.SLIST["SS"+id] = True
+        except:
+            FIREBASE_GLOBAL_VAR.SLIST["SS"+id] = True
+        INTERNAL.connect(id)
+        LOCAL_SS = FIREBASE_GLOBAL_VAR.SLIST["SS"+id]
+        LOCAL_SS.write(b"PUT /"+PATH+b".json HTTP/1.0\r\n")
+        LOCAL_SS.write(
+            b"Host: "+FIREBASE_GLOBAL_VAR.GLOBAL_URL_ADINFO["host"]+b"\r\n")
+        LOCAL_SS.write(b"Content-Length: "+str(len(DATA))+"\r\n\r\n")
+        LOCAL_SS.write(DATA)
+        LOCAL_DUMMY = LOCAL_SS.read()
+        del LOCAL_DUMMY
+        INTERNAL.disconnect(id)
+        if cb:
+            try:
+                cb[0](*cb[1])
+            except:
+                try:
+                    cb[0](cb[1])
+                except:
+                    raise OSError(
+                        "Callback function could not be executed. Try the function without ufirebase.py callback.")
+
+    def patch(PATH, DATATAG, id, cb):
+        try:
+            while FIREBASE_GLOBAL_VAR.SLIST["SS"+id]:
+                time.sleep(1)
+            FIREBASE_GLOBAL_VAR.SLIST["SS"+id] = True
+        except:
+            FIREBASE_GLOBAL_VAR.SLIST["SS"+id] = True
+        INTERNAL.connect(id)
+        LOCAL_SS = FIREBASE_GLOBAL_VAR.SLIST["SS"+id]
+        LOCAL_SS.write(b"PATCH /"+PATH+b".json HTTP/1.0\r\n")
+        LOCAL_SS.write(
+            b"Host: "+FIREBASE_GLOBAL_VAR.GLOBAL_URL_ADINFO["host"]+b"\r\n")
+        LOCAL_SS.write(b"Content-Length: "+str(len(DATATAG))+"\r\n\r\n")
+        LOCAL_SS.write(DATATAG)
+        LOCAL_DUMMY = LOCAL_SS.read()
+        del LOCAL_DUMMY
+        INTERNAL.disconnect(id)
+        if cb:
+            try:
+                cb[0](*cb[1])
+            except:
+                try:
+                    cb[0](cb[1])
+                except:
+                    raise OSError(
+                        "Callback function could not be executed. Try the function without ufirebase.py callback.")
+
+    def get(PATH, DUMP, id, cb, limit):
+        try:
+            while FIREBASE_GLOBAL_VAR.SLIST["SS"+id]:
+                time.sleep(1)
+            FIREBASE_GLOBAL_VAR.SLIST["SS"+id] = True
+        except:
+            FIREBASE_GLOBAL_VAR.SLIST["SS"+id] = True
+        INTERNAL.connect(id)
+        LOCAL_SS = FIREBASE_GLOBAL_VAR.SLIST["SS"+id]
+        LOCAL_SS.write(b"GET /"+PATH+b".json?shallow=" +
+                       ujson.dumps(limit)+b" HTTP/1.0\r\n")
+        LOCAL_SS.write(
+            b"Host: "+FIREBASE_GLOBAL_VAR.GLOBAL_URL_ADINFO["host"]+b"\r\n\r\n")
+        LOCAL_OUTPUT = ujson.loads(LOCAL_SS.read().splitlines()[-1])
+        INTERNAL.disconnect(id)
+        globals()[DUMP] = LOCAL_OUTPUT
+        if cb:
+            try:
+                cb[0](*cb[1])
+            except:
+                try:
+                    cb[0](cb[1])
+                except:
+                    raise OSError(
+                        "Callback function could not be executed. Try the function without ufirebase.py callback.")
+
+    def getfile(PATH, FILE, bg, id, cb, limit):
+        try:
+            while FIREBASE_GLOBAL_VAR.SLIST["SS"+id]:
+                time.sleep(1)
+            FIREBASE_GLOBAL_VAR.SLIST["SS"+id] = True
+        except:
+            FIREBASE_GLOBAL_VAR.SLIST["SS"+id] = True
+        INTERNAL.connect(id)
+        LOCAL_SS = FIREBASE_GLOBAL_VAR.SLIST["SS"+id]
+        LOCAL_SS.write(b"GET /"+PATH+b".json?shallow=" +
+                       ujson.dumps(limit)+b" HTTP/1.0\r\n")
+        LOCAL_SS.write(
+            b"Host: "+FIREBASE_GLOBAL_VAR.GLOBAL_URL_ADINFO["host"]+b"\r\n\r\n")
+        while not LOCAL_SS.readline() == b"\r\n":
+            pass
+        LOCAL_FILE = open(FILE, "wb")
+        if bg:
+            while True:
+                LOCAL_LINE = LOCAL_SS.read(1024)
+                if LOCAL_LINE == b"":
+                    break
+                LOCAL_FILE.write(LOCAL_LINE)
+                time.sleep_ms(1)
+        else:
+            while True:
+                LOCAL_LINE = LOCAL_SS.read(1024)
+                if LOCAL_LINE == b"":
+                    break
+                LOCAL_FILE.write(LOCAL_LINE)
+        LOCAL_FILE.close()
+        LOCAL_DUMMY = LOCAL_SS.read()
+        del LOCAL_DUMMY
+        INTERNAL.disconnect(id)
+        if cb:
+            try:
+                cb[0](*cb[1])
+            except:
+                try:
+                    cb[0](cb[1])
+                except:
+                    raise OSError(
+                        "Callback function could not be executed. Try the function without ufirebase.py callback.")
+
+    def delete(PATH, id, cb):
+        try:
+            while FIREBASE_GLOBAL_VAR.SLIST["SS"+id]:
+                time.sleep(1)
+            FIREBASE_GLOBAL_VAR.SLIST["SS"+id] = True
+        except:
+            FIREBASE_GLOBAL_VAR.SLIST["SS"+id] = True
+        INTERNAL.connect(id)
+        LOCAL_SS = FIREBASE_GLOBAL_VAR.SLIST["SS"+id]
+        LOCAL_SS.write(b"DELETE /"+PATH+b".json HTTP/1.0\r\n")
+        LOCAL_SS.write(
+            b"Host: "+FIREBASE_GLOBAL_VAR.GLOBAL_URL_ADINFO["host"]+b"\r\n\r\n")
+        LOCAL_DUMMY = LOCAL_SS.read()
+        del LOCAL_DUMMY
+        INTERNAL.disconnect(id)
+        if cb:
+            try:
+                cb[0](*cb[1])
+            except:
+                try:
+                    cb[0](cb[1])
+                except:
+                    raise OSError(
+                        "Callback function could not be executed. Try the function without ufirebase.py callback.")
+
+    def addto(PATH, DATA, DUMP, id, cb):
+        try:
+            while FIREBASE_GLOBAL_VAR.SLIST["SS"+id]:
+                time.sleep(1)
+            FIREBASE_GLOBAL_VAR.SLIST["SS"+id] = True
+        except:
+            FIREBASE_GLOBAL_VAR.SLIST["SS"+id] = True
+        INTERNAL.connect(id)
+        LOCAL_SS = FIREBASE_GLOBAL_VAR.SLIST["SS"+id]
+        LOCAL_SS.write(b"POST /"+PATH+b".json HTTP/1.0\r\n")
+        LOCAL_SS.write(
+            b"Host: "+FIREBASE_GLOBAL_VAR.GLOBAL_URL_ADINFO["host"]+b"\r\n")
+        LOCAL_SS.write(b"Content-Length: "+str(len(DATA))+"\r\n\r\n")
+        LOCAL_SS.write(DATA)
+        LOCAL_OUTPUT = ujson.loads(LOCAL_SS.read().splitlines()[-1])
+        INTERNAL.disconnect(id)
+        if DUMP:
+            globals()[DUMP] = LOCAL_OUTPUT["name"]
+        if cb:
+            try:
+                cb[0](*cb[1])
+            except:
+                try:
+                    cb[0](cb[1])
+                except:
+                    raise OSError(
+                        "Callback function could not be executed. Try the function without ufirebase.py callback.")
+
+
+def setURL(url):
+    FIREBASE_GLOBAL_VAR.GLOBAL_URL = url
+    try:
+        proto, dummy, host, path = url.split("/", 3)
+    except ValueError:
+        proto, dummy, host = url.split("/", 2)
+        path = ""
+    if proto == "http:":
+        port = 80
+    elif proto == "https:":
+        import ussl
+        port = 443
+    else:
+        raise ValueError("Unsupported protocol: " + proto)
+
+    if ":" in host:
+        host, port = host.split(":", 1)
+        port = int(port)
+
+    FIREBASE_GLOBAL_VAR.GLOBAL_URL_ADINFO = {
+        "proto": proto, "host": host, "port": port}
+
+
+def put(PATH, DATA, bg=True, id=0, cb=None):
+    if bg:
+        _thread.start_new_thread(
+            INTERNAL.put, [PATH, ujson.dumps(DATA), str(id), cb])
+    else:
+        INTERNAL.put(PATH, ujson.dumps(DATA), str(id), cb)
+
+
+def patch(PATH, DATATAG, bg=True, id=0, cb=None):
+    if bg:
+        _thread.start_new_thread(
+            INTERNAL.patch, [PATH, ujson.dumps(DATATAG), str(id), cb])
+    else:
+        INTERNAL.patch(PATH, ujson.dumps(DATATAG), str(id), cb)
+
+
+def getfile(PATH, FILE, bg=False, id=0, cb=None, limit=False):
+    if bg:
+        _thread.start_new_thread(
+            INTERNAL.getfile, [PATH, FILE, bg, str(id), cb, limit])
+    else:
+        INTERNAL.getfile(PATH, FILE, bg, str(id), cb, limit)
+
+
+def get(PATH, DUMP, bg=False, cb=None, id=0, limit=False):
+    if bg:
+        _thread.start_new_thread(
+            INTERNAL.get, [PATH, DUMP, str(id), cb, limit])
+    else:
+        INTERNAL.get(PATH, DUMP, str(id), cb, limit)
+
+
+def delete(PATH, bg=True, id=0, cb=None):
+    if bg:
+        _thread.start_new_thread(INTERNAL.delete, [PATH, str(id), cb])
+    else:
+        INTERNAL.delete(PATH, str(id), cb)
+
+
+def addto(PATH, DATA, DUMP=None, bg=True, id=0, cb=None):
+    if bg:
+        _thread.start_new_thread(
+            INTERNAL.addto, [PATH, ujson.dumps(DATA), DUMP, str(id), cb])
+    else:
+        INTERNAL.addto(PATH, ujson.dumps(DATA), DUMP, str(id), cb)
