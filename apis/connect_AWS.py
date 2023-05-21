af28f0e9507068f629c69e5a85c38193976102cf
fawrama <bodygawad9@gmail.com>
2023-05-22 01:29:56 +0800

    Changes to be committed: 	new file:   apis/__pycache__/connect_supabase.cpython-310.pyc 	new file:   apis/connect_AWS.py 	new file:   apis/connect_supabase.py 	new file:   apis/ecg_prediction/fingerprint.pb 	new file:   apis/ecg_prediction/keras_metadata.pb 	new file:   apis/ecg_prediction/saved_model.pb 	new file:   apis/ecg_prediction/variables/variables.data-00000-of-00001 	new file:   apis/ecg_prediction/variables/variables.index 	new file:   apis/stroke prediction models/model_Decision Tree 	new file:   apis/stroke prediction models/model_KNN 	new file:   apis/stroke prediction models/model_Random Forest 	new file:   apis/stroke prediction models/model_SVM

--- apis/connect_AWS.py
+++ apis/connect_AWS.py
@@ -1,16 +1,41 @@
-import boto3
 import os
+from tensorflow import keras
+import pickle
+import connect_supabase
+import pandas as pd
+import numpy as np
+# set credentials in heroku as environment variables
 
-accessKey: str = os.environ.get("awsAccessKey")
-secretKey: str = os.environ.get("awsSecretKey")
+# download the s3 bucket in local heroku environment
 
-session = boto3.Session(
-    aws_access_key_id=accessKey,
-    aws_secret_access_key=secretKey,
-    region_name= 'ap-southeast-1'
-)
+os.system('aws s3 sync s3://userdatastrokeprediction .')
 
-s3 = session.resource('s3')
+# laod the models
+ecg_model = keras.models.load_model('ecg_prediction/')
+stroke_models = []
+models = os.listdir('stroke prediction models/')
+for i in models:
+    stroke_models.append(pickle.load(
+        open(f'stroke prediction models/{i}', 'rb')))
+# fetch the data from supabase using the user_id
+ecgReadings = connect_supabase.getUserData(
+    '3a283a5f-a00a-4a8e-ba7b-30e3e72bb7f6')
+ecgReadingsDf = pd.DataFrame(ecgReadings).T
+ecgReadingsDf.columns = list(range(187))
+# predict functions
 
-buckets = [bucket.name for bucket in s3.buckets.all()]
-print(buckets)
+
+def predict_heart_disease(ecg_model, ecgReadings):
+    prediction = ecg_model.predict(ecgReadings)
+    return np.array(prediction).argmax()
+
+
+def predict_stroke(stroke_models, predicted_heart):
+    stroke_prediction = []
+    for i in stroke_models:
+        stroke_prediction.append(i.predict())
+# return predicted values to supabase to be read from the front end
+
+
+print("heart disease predicted:", predict_heart_disease(
+    ecg_model=ecg_model, ecgReadings=ecgReadingsDf))
--- apis/connect_supabase.py
+++ apis/connect_supabase.py
@@ -1,15 +1,14 @@
-
-
 import os
 from supabase import create_client, Client
 
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
-supabase: Client = create_client(supabase_url=  url,supabase_key= key)
+supabase: Client = create_client(supabase_url=url, supabase_key=key)
 
 
-response = supabase.table('ecg').select("*").execute()
-
-print(response)
-
-
+def getUserData(user_id):
+    res = supabase.table('ecg').select('*').eq(
+        'user_id',
+        user_id
+    ).execute()
+    return res.data[3]['values'][-187:]
