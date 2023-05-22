18779bbb259085a42735580484dbd587e548d3d2
Abdullah Abdelgawad <71787197+fawrama@users.noreply.github.com>
2023-05-22 11:23:13 +0300

    Update connect_supabase.py

--- apis/connect_supabase.py
+++ apis/connect_supabase.py
@@ -5,19 +5,10 @@ url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(supabase_url=url, supabase_key=key)
 
-<<<<<<< HEAD
-=======
+
 def getUserData(user_id):
 	res = supabase.table('ecg').select('*').eq(
 		'user_id',
 	    user_id
 		).execute()
-	return res.data[3]['values'][-187:]
->>>>>>> 101d141 (get user readings from supabase)
-
-def getUserData(user_id):
-    res = supabase.table('ecg').select('*').eq(
-        'user_id',
-        user_id
-    ).execute()
-    return res.data[3]['values'][-187:]
+	return res.data[-1]['values'][-187:]
