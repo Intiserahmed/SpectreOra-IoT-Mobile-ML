04f44eabfc9f7728f4a7fa45073abd53c41a2971
fawrama <bodygawad9@gmail.com>
2023-05-24 09:03:09 +0800

    modified:   apis/__pycache__/fastApi.cpython-310.pyc 	deleted:    apis/ecg_prediction/fingerprint.pb 	deleted:    apis/ecg_prediction/keras_metadata.pb 	deleted:    apis/ecg_prediction/saved_model.pb 	deleted:    apis/ecg_prediction/variables/variables.data-00000-of-00001 	deleted:    apis/ecg_prediction/variables/variables.index 	modified:   apis/fastApi.py 	deleted:    apis/stroke prediction models/model_Decision Tree 	deleted:    apis/stroke prediction models/model_KNN 	deleted:    apis/stroke prediction models/model_Random Forest 	deleted:    apis/stroke prediction models/model_SVM

--- apis/fastApi.py
+++ apis/fastApi.py
@@ -3,15 +3,17 @@ import connect_AWS
 import json
 
 app = FastAPI()
+
+
 @app.get("/")
 async def getPredictions(userId):
     if userId == '':
         raise HTTPException(status_code=404, detail="userID is not provided")
     predictedHeartDisease = connect_AWS.predict_heart_disease(userId)
     if predictedHeartDisease:
-	    predictedHeartDiseaseJson = '{"predicted heart disease": "True"}'
-	    
+        predictedHeartDiseaseJson = '{"predicted heart disease": "True"}'
+
     else:
-	    predictedHeartDiseaseJson = '{"predicted heart disease": "False"}'
-    
+        predictedHeartDiseaseJson = '{"predicted heart disease": "False"}'
+
     return json.loads(predictedHeartDiseaseJson)
