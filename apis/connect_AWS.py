86fbb2cc5da184722ca57d73ca0410f642b1ff5d
fawrama <bodygawad9@gmail.com>
2023-05-24 09:01:33 +0800

    Changes to be committed: 	new file:   apis/__pycache__/connect_AWS.cpython-310.pyc 	new file:   apis/__pycache__/connect_supabase.cpython-310.pyc 	new file:   apis/__pycache__/fastApi.cpython-310.pyc 	modified:   apis/connect_AWS.py 	modified:   apis/connect_supabase.py 	new file:   apis/ecg_prediction/fingerprint.pb 	new file:   apis/ecg_prediction/keras_metadata.pb 	new file:   apis/ecg_prediction/saved_model.pb 	new file:   apis/ecg_prediction/variables/variables.data-00000-of-00001 	new file:   apis/ecg_prediction/variables/variables.index 	new file:   apis/fastApi.py 	new file:   apis/stroke prediction models/model_Decision Tree 	new file:   apis/stroke prediction models/model_KNN 	new file:   apis/stroke prediction models/model_Random Forest 	new file:   apis/stroke prediction models/model_SVM
    Changes not staged for commit:
    	modified:   apis/__pycache__/fastApi.cpython-310.pyc
    	deleted:    apis/ecg_prediction/fingerprint.pb
    	deleted:    apis/ecg_prediction/keras_metadata.pb
    	deleted:    apis/ecg_prediction/saved_model.pb
    	deleted:    apis/ecg_prediction/variables/variables.data-00000-of-00001
    	deleted:    apis/ecg_prediction/variables/variables.index
    	modified:   apis/fastApi.py
    	deleted:    apis/stroke prediction models/model_Decision Tree
    	deleted:    apis/stroke prediction models/model_KNN
    	deleted:    apis/stroke prediction models/model_Random Forest
    	deleted:    apis/stroke prediction models/model_SVM

--- apis/connect_AWS.py
+++ apis/connect_AWS.py
@@ -11,31 +11,58 @@ import numpy as np
 os.system('aws s3 sync s3://userdatastrokeprediction .')
 
 # laod the models
-ecg_model = keras.models.load_model('ecg_prediction/')
-stroke_models = []
-models = os.listdir('stroke prediction models/')
-for i in models:
-    stroke_models.append(pickle.load(
-        open(f'stroke prediction models/{i}', 'rb')))
-# fetch the data from supabase using the user_id
-ecgReadings = connect_supabase.getUserData(
-    '3a283a5f-a00a-4a8e-ba7b-30e3e72bb7f6')
-ecgReadingsDf = pd.DataFrame(ecgReadings).T
-ecgReadingsDf.columns = list(range(187))
+def loadECGModel():
+    ecg_model = keras.models.load_model('ecg_prediction/')
+    stroke_models = []
+    models = os.listdir('stroke prediction models/')
+    for i in models:
+        stroke_models.append(pickle.load(
+            open(f'stroke prediction models/{i}', 'rb')))
+    return ecg_model
+    # fetch the data from supabase using the user_id
+def getUserReadings(userId):
+    ecgReadings = connect_supabase.getUserData(userId)
+    ecgReadingsDf = pd.DataFrame(ecgReadings).T
+    ecgReadingsDf.columns = list(range(187))
+    return ecgReadingsDf
+
+
+
+
+
+# user details as a variable till the app is ready & preprocessing 
+# user_details = {
+#     'gender': 'male',
+#     'age': 24,
+#     'hypertension':1,
+#     'ever_married': 'Yes',
+#     'work_type': 'Private',
+#     'Residence_type': 1,
+#     'avg_glucose_level': 120,
+#     'bmi': 21.5,
+#     'smoking_status': 'formerly smoked'
+# }
+
 # predict functions
 
 
-def predict_heart_disease(ecg_model, ecgReadings):
-    prediction = ecg_model.predict(ecgReadings)
+def predict_heart_disease(userID):
+    ecgModel = loadECGModel()
+    ecgReadings = getUserReadings(userID)
+    prediction = ecgModel.predict(ecgReadings)
     return np.array(prediction).argmax()
 
 
-def predict_stroke(stroke_models, predicted_heart):
-    stroke_prediction = []
-    for i in stroke_models:
-        stroke_prediction.append(i.predict())
+def predict_stroke(stroke_models, predicted_heart, user_details):
+    stroke_predictions = []
+    for model in stroke_models:
+        stroke_prediction.append(model.predict())
 # return predicted values to supabase to be read from the front end
 
 
-print("heart disease predicted:", predict_heart_disease(
-    ecg_model=ecg_model, ecgReadings=ecgReadingsDf))
+# print("Storke predicted:", predict_stroke(
+#     stroke_models=stroke_models,
+#     predicted_haert_disease=predict_heart_disease(ecg_model, ecgReadings),
+#     user_details= processed_data
+# )
+# )
--- apis/connect_supabase.py
+++ apis/connect_supabase.py
@@ -5,10 +5,19 @@ url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(supabase_url=url, supabase_key=key)
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 3d051bb (Changes to be committed:)
 def getUserData(user_id):
 	res = supabase.table('ecg').select('*').eq(
 		'user_id',
 	    user_id
 		).execute()
+<<<<<<< HEAD
 	return res.data[-1]['values'][-187:]
+=======
+	return res.data[3]['values'][-187:]
+
+
+>>>>>>> 3d051bb (Changes to be committed:)
