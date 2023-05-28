d99ad6315fed0ce29fa85c53ee2baa6f48e2704e
fawrama <bodygawad9@gmail.com>
2023-05-28 14:43:51 +0800

    modified:   apis/connect_AWS.py 	deleted:    apis/ecg_prediction/fingerprint.pb 	deleted:    apis/ecg_prediction/keras_metadata.pb 	deleted:    apis/ecg_prediction/saved_model.pb 	deleted:    apis/ecg_prediction/variables/variables.data-00000-of-00001 	deleted:    apis/ecg_prediction/variables/variables.index 	deleted:    apis/fullprocessor 	deleted:    apis/stroke prediction models/model_Decision Tree 	deleted:    apis/stroke prediction models/model_KNN 	deleted:    apis/stroke prediction models/model_Random Forest

--- apis/connect_AWS.py
+++ apis/connect_AWS.py
@@ -70,7 +70,7 @@ def predict_stroke(stroke_models, gender, age, hyperTension, predictedHeartDisea
     for i in stroke_predictions:
         avg += i[0][1]
 #         print(i[0][1])
-    avg /= 3
+    avg /= 2
     return avg*100
 
 # return predicted values to supabase to be read from the front end
